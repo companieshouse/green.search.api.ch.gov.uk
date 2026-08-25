07d2e22a1da09d99d143ade1c6e2469d8454d884
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2026-08-25 21:35:09 +0100

    WIP: intermediate state

--- src/test/java/uk/gov/companieshouse/search/api/controller/AlphabeticalSearchControllerTest.java
+++ src/test/java/uk/gov/companieshouse/search/api/controller/AlphabeticalSearchControllerTest.java
@@ -1,24 +1,33 @@
 package uk.gov.companieshouse.search.api.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.search.api.mapper.ApiToResponseMapper;
 import uk.gov.companieshouse.search.api.model.SearchResults;
 import uk.gov.companieshouse.search.api.model.data.Company;
 import uk.gov.companieshouse.search.api.model.response.ResponseObject;
 import uk.gov.companieshouse.search.api.model.response.ResponseStatus;
+import uk.gov.companieshouse.search.api.service.delete.alphabetical.AlphabeticalSearchDeleteService;
 import uk.gov.companieshouse.search.api.service.search.SearchIndexService;
+import uk.gov.companieshouse.search.api.service.upsert.UpsertCompanyService;
 import uk.gov.companieshouse.search.api.util.ConfiguredIndexNamesProvider;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AlphabeticalSearchController.class)
@@ -29,6 +38,12 @@ class AlphabeticalSearchControllerTest {
 
     @MockitoBean
     private SearchIndexService searchIndexService;
+
+    @MockitoBean
+    private UpsertCompanyService upsertCompanyService;
+
+    @MockitoBean
+    private AlphabeticalSearchDeleteService alphabeticalSearchDeleteService;
 
     @MockitoBean
     private ApiToResponseMapper apiToResponseMapper;
@@ -74,5 +89,69 @@ class AlphabeticalSearchControllerTest {
         mockMvc.perform(get("/alphabetical-search/companies")
                         .param("q", "test company"))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void upsertCompanySuccessfully() throws Exception {
+        CompanyProfileApi company = new CompanyProfileApi();
+        company.setCompanyNumber("12345678");
+        company.setCompanyName("Test Company Ltd");
+
+        ResponseObject<String> responseObject = new ResponseObject<>(ResponseStatus.DOCUMENT_UPSERTED);
+
+        Mockito.when(upsertCompanyService.upsert(any(CompanyProfileApi.class)))
+                .thenReturn(responseObject);
+
+        Mockito.when(apiToResponseMapper.map(any()))
+                .thenReturn(ResponseEntity.ok().contentType(APPLICATION_JSON).body(responseObject));
+
+        mockMvc.perform(put("/alphabetical-search/companies/12345678")
+                        .contentType(APPLICATION_JSON)
+                        .content(asJsonString(company)))
+                .andExpect(status().isOk());
+
+        verify(upsertCompanyService, Mockito.times(1)).upsert(any(CompanyProfileApi.class));
+    }
+
+    @Test
+    void upsertCompanyWithMismatchedNumberReturnsBadRequest() throws Exception {
+        CompanyProfileApi company = new CompanyProfileApi();
+        company.setCompanyNumber("12345678");
+        company.setCompanyName("Test Company Ltd");
+
+        ResponseObject<String> responseObject = new ResponseObject<>(ResponseStatus.UPSERT_ERROR);
+
+        Mockito.when(apiToResponseMapper.map(any()))
+                .thenReturn(ResponseEntity.ok().contentType(APPLICATION_JSON).body(responseObject));
+
+        ResultActions resultActions = mockMvc.perform(put("/alphabetical-search/companies/87654321")
+                        .contentType(APPLICATION_JSON)
+                        .content(asJsonString(company)))
+                .andExpect(status().isOk());
+
+        assertTrue(resultActions.andReturn().getResponse().getContentAsString().contains(ResponseStatus.UPSERT_ERROR.name()));
+
+        verify(upsertCompanyService, Mockito.never()).upsert(any());
+    }
+
+    @Test
+    void upsertCompanyWithEmptyNumberReturnsBadRequest() throws Exception {
+        CompanyProfileApi company = new CompanyProfileApi();
+        company.setCompanyNumber("12345678");
+        company.setCompanyName("Test Company Ltd");
+
+        ResponseObject<String> responseObject = new ResponseObject<>(ResponseStatus.UPSERT_ERROR);
+
+        Mockito.when(apiToResponseMapper.map(any()))
+                .thenReturn(ResponseEntity.ok().contentType(APPLICATION_JSON).body(responseObject));
+
+        mockMvc.perform(put("/alphabetical-search/companies/")
+                        .contentType(APPLICATION_JSON)
+                        .content(asJsonString(company)))
+                .andExpect(status().isNotFound());
+    }
+
+    private String asJsonString(final Object obj) throws Exception {
+        return new ObjectMapper().writeValueAsString(obj);
     }
 }
